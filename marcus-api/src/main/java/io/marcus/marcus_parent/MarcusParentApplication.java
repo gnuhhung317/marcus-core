2611511b525d85c4f83f1af422d6fac03c127d7f
Duc Hung <duchung04st@gmail.com>
2026-03-05 16:17:06 +0700

    init project

---
+++ marcus-api/src/main/java/io/marcus/marcus_parent/MarcusParentApplication.java
@@ -0,0 +1,13 @@
+package io.marcus.marcus_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcusParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcusParentApplication.class, args);
+	}
+
+}
---
+++ marcus-api/src/test/java/io/marcus/marcus_parent/MarcusParentApplicationTests.java
@@ -0,0 +1,15 @@
+package io.marcus.marcus_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Disabled;
+
+@SpringBootTest
+@Disabled("No DB config in application.yaml yet")
+class MarcusParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
